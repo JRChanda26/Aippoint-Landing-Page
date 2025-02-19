97bbae35c7d48c981fa835615798185c407e9614
Iosys <amit.p@iosys.in>
2025-02-19 18:03:41 +0530

    new page added

--- src/app/mainpage/Hirethebest.tsx
+++ src/app/mainpage/Hirethebest.tsx
@@ -17,8 +17,8 @@ function Hire() {
       <Grid container spacing={0}>
         <Grid item lg={6}>
           {hirePage.map((post: any) => (
-            <>
-              <div key={post.id} style={{paddingLeft:'20px',paddingTop:'283px'}}>
+            <div key={post.id}>
+              <div style={{ paddingLeft: "20px", paddingTop: "283px" }}>
                 <Typography
                   style={{
                     fontFamily: "Poppins",
@@ -31,64 +31,76 @@ function Hire() {
                   {post.data.heading}
                 </Typography>
               </div>
-              <div style={{paddingLeft:'20px'}}>
-                <Typography style={{
+              <div style={{ paddingLeft: "20px" }}>
+                <Typography
+                  style={{
                     fontFamily: "Poppins",
                     fontWeight: 400,
                     fontSize: "18px",
                     lineHeight: "27px",
                     color: "#0A0A0A",
-                  }}>{post.data.content}</Typography>
-              </div>
-              <div style={{display:'flex',gap:'24px',paddingLeft:'20px',paddingTop:'25px'}}>
-              <div style={{}}>
-                <Button
-                  style={{
-                    textTransform: "none",
-                    background: "#0083C3",
-                    borderRadius: "12px",
-                    color: "#FFFFFF",
-                    padding:'18px 58px 18px 59px'
                   }}
                 >
-                  {post.data.getstarted_button}
-                </Button>
+                  {post.data.content}
+                </Typography>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  gap: "24px",
+                  paddingLeft: "20px",
+                  paddingTop: "25px",
+                }}
+              >
+                <div style={{}}>
+                  <Button
+                    style={{
+                      textTransform: "none",
+                      background: "#0083C3",
+                      borderRadius: "12px",
+                      color: "#FFFFFF",
+                      padding: "18px 58px 18px 59px",
+                    }}
+                  >
+                    {post.data.getstarted_button}
+                  </Button>
                 </div>
                 <div style={{}}>
-                <Button
-                  style={{
-                    textTransform: "none",
-                    borderRadius: "12px",
-                    color: "#0A0A0A",
-                    border: "1px solid #0A0A0A",
-                    padding:'18px 38px 18px 38px'
-                  }}
-                >
-                  {post.data.requestdemo_button}
-                </Button>
+                  <Button
+                    style={{
+                      textTransform: "none",
+                      borderRadius: "12px",
+                      color: "#0A0A0A",
+                      border: "1px solid #0A0A0A",
+                      padding: "18px 38px 18px 38px",
+                    }}
+                  >
+                    {post.data.requestdemo_button}
+                  </Button>
                 </div>
-                </div>
-              
-            </>
+              </div>
+            </div>
           ))}
         </Grid>
         <Grid item lg={6}>
           {hirePage.map((post: any) => (
-            <div key={post.id} style={{paddingTop:'40px'}}>
-              {post?.data.hire_image && (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                  src={post.data.hire_image.url || undefined}
-                  alt={post.data.hire_image.alt || "Image"}
-                  style={
-                    {
-                      //   width: "166.31px",
-                      //   height: "46px",
-                      //   cursor: "pointer",
+            <div key={post.id}>
+              <div style={{ paddingTop: "40px" }}>
+                {post?.data.hire_image && (
+                  // eslint-disable-next-line @next/next/no-img-element
+                  <img
+                    src={post.data.hire_image.url || undefined}
+                    alt={post.data.hire_image.alt || "Image"}
+                    style={
+                      {
+                        //   width: "166.31px",
+                        //   height: "46px",
+                        //   cursor: "pointer",
+                      }
                     }
-                  }
-                />
-              )}
+                  />
+                )}
+              </div>
             </div>
           ))}
         </Grid>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Contact from "./mainpage/contact";
 import Footer from "./mainpage/footer";
 import Header from "./mainpage/header";
 import Hire from "./mainpage/Hirethebest";
+import Ourvalueproposition from "./mainpage/Ourvalueproposition";
+import Revolutionizing from "./mainpage/Revolutionizing";
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Hire/>
+      <Ourvalueproposition/>
+      <Revolutionizing/>
       <Contact/>
       <Footer/>
     </div>
